c9c3915bb6290e7bfb0c1fbb49a85e04e2ea4b4a
Jakob Nilsson <jakob.a.nilsson@gmail.com>
2020-07-06 16:25:26 +0200

    Added encoder buttons

--- Code/led_game_buttons_and_sound/led_game_buttons_and_sound.ino
+++ Code/led_game_buttons_and_sound/led_game_buttons_and_sound.ino
@@ -5,8 +5,9 @@ Documentation at
 https://github.com/Jaknil/LED_games/blob/master/README.md
 */
 
-const String buttons[] = {"OPTIONS", "START","UP","RIGHT","ACTION","DOWN","LEFT"};
-const int InputPins[] = {8,9,10,11,A0,A4,A5}; // 1K Hardware pull down and positive logic
+const String buttons[] = {"OPTIONS", "START","UP","RIGHT","ACTION","DOWN","LEFT","ENC1S","ENC2S"};
+const int InputPins[] = {8,9,10,11,A0,A4,A5,4,7}; // 1K Hardware pull down and positive logic for most, encoders are inverted
+int buttonStatus[sizeof(buttons) / sizeof(buttons[0])];
 
 //OUTPUTS
 #define LEDPin 13      // the number of the onboard LED OUTPUT pin 
@@ -29,19 +30,27 @@ void loop() {
 
 //Step through and read all button pins
 for (int i = 0; i < (sizeof(buttons) / sizeof(buttons[0])); i++) {
+buttonStatus[i] = digitalRead(InputPins[i]);
 
-if (digitalRead(InputPins[i]) == HIGH){
-  Serial.println(buttons[i]);
+//invert the encoder pins
+buttonStatus[7] = !buttonStatus[7];
+buttonStatus[8] = !buttonStatus[8];
+//do something with the data
+if (buttonStatus[i] == HIGH){
+  Serial.println(buttons[i]); //print the pressed button to serial
   digitalWrite(LEDPin, HIGH);
-  digitalWrite(SPKbuttonPin, HIGH); //test speaker when button is pressed
+}
+}
+
+//test speaker
+
+  if(buttonStatus[0]==1 && buttonStatus[1]==1){ //test speaker when both small buttons are pressed
+  digitalWrite(SPKbuttonPin, HIGH); 
   delay(10); //short since the speaker is loud
     }else {
         // turn LED and speaker off:
               digitalWrite(LEDPin, LOW);
               digitalWrite(SPKbuttonPin, LOW);
-
   }
- 
-}
- 
+
 }
