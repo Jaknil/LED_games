e58d5e4ed3c2f10ebd6dab1bbc3bdf0ca2978756
Jakob Nilsson <jakob.a.nilsson@gmail.com>
2020-07-12 23:01:58 +0200

    created function to scan buttons

--- Code/led_game_buttons_and_sound/led_game_buttons_and_sound.ino
+++ Code/led_game_buttons_and_sound/led_game_buttons_and_sound.ino
@@ -5,12 +5,15 @@ Documentation at
 https://github.com/Jaknil/LED_games/blob/master/README.md
 */
 
+//Button variables and constants
 const String buttons[] = {"OPTIONS", "START","UP","RIGHT","ACTION","DOWN","LEFT","ENC1S","ENC2S"};
 const int InputPins[] = {8,9,10,11,A0,A4,A5,4,7}; // 1K Hardware pull down and positive logic for most, encoders are inverted
-int buttonStatus[sizeof(buttons) / sizeof(buttons[0])];
-
+const int countButtons (sizeof(buttons) / sizeof(buttons[0])); //saves some MCU time
+int buttonStatus[countButtons];
 //OUTPUTS
 #define LEDPin 13      // the number of the onboard LED OUTPUT pin 
+
+//Speaker pin
 #define SPKbuttonPin A3     // the number of the SPK speaker OUTPUT pin (active with 150Ohm series resistor)
 
 void setup() {
@@ -21,23 +24,20 @@ void setup() {
   pinMode(SPKbuttonPin, OUTPUT);
   
   // initialize the button pins as inputs:
-  for (int i = 0; i < (sizeof(InputPins) / sizeof(InputPins[0])); i++) {
+  for (int i = 0; i < (countButtons); i++) {
       pinMode(InputPins[i], INPUT);
   }
 }
 
 void loop() {
 
-//Step through and read all button pins
-for (int i = 0; i < (sizeof(buttons) / sizeof(buttons[0])); i++) {
-buttonStatus[i] = digitalRead(InputPins[i]);
+ScanButtons(); //Scans all buttons
 
-//invert the encoder pins
-buttonStatus[7] = !buttonStatus[7];
-buttonStatus[8] = !buttonStatus[8];
 //do something with the data
+for (int i = 0; i < (countButtons); i++) {
 if (buttonStatus[i] == HIGH){
   Serial.println(buttons[i]); //print the pressed button to serial
+  delay(50);
 }
 }
 
@@ -49,7 +49,6 @@ if (buttonStatus[i] == HIGH){
     digitalWrite(LEDPin, LOW);
   }
 //test speaker
-
   if(buttonStatus[0]==HIGH && buttonStatus[1]==HIGH){ //test speaker when both small buttons are pressed
   digitalWrite(SPKbuttonPin, HIGH); 
   delay(10); //short since the speaker is loud
@@ -58,3 +57,16 @@ if (buttonStatus[i] == HIGH){
                             digitalWrite(SPKbuttonPin, LOW);
   }
 }
+
+void ScanButtons(){
+//Step through and read all button pins
+for (int i = 0; i < (countButtons); i++) {
+buttonStatus[i] = digitalRead(InputPins[i]);
+}
+//invert the encoder pins since they have inverted logic
+buttonStatus[7] = !buttonStatus[7];
+buttonStatus[8] = !buttonStatus[8];
+return;
+}
+
+  
